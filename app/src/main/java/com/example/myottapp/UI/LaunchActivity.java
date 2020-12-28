038e17436281b7a79bc187f27decfb8d5462935a
darkPassenger-m <m7.manohar@gmail.com>
2020-12-28 15:59:47 +0530

    Session-getSession

--- app/src/main/java/com/example/myottapp/UI/LaunchActivity.java
+++ app/src/main/java/com/example/myottapp/UI/LaunchActivity.java
@@ -9,13 +9,24 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ChallengeContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.MultiFactorAuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.AuthenticationHandler;
 import com.example.myottapp.R;
 import com.example.myottapp.models.SessionManager;
+import com.example.myottapp.models.UserDetails;
 
 public class LaunchActivity extends Activity {
     ProgressBar progressBar;
     SessionManager sessionManager;
+    UserDetails userDetails;
+    String email,password;
+
     private  static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +39,60 @@ public class LaunchActivity extends Activity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                sessionManager.checkLogin();
+                if (sessionManager.checkLogin()){
+                    email=sessionManager.getEMAIL();
+                    password = sessionManager.getPASSWORD();
+                    login(email, password);
+                }
             }
         },SPLASH_TIME_OUT);
+
+
+
+    }
+    final AuthenticationHandler authenticationHandler = new AuthenticationHandler() {
+        @Override
+        public void onSuccess(CognitoUserSession userSession, CognitoDevice newDevice) {
+            progressBar.setVisibility(View.GONE);
+            System.out.println("Login successfull");
+            System.out.println(userSession.getAccessToken().getExpiration());
+            CognitoSettings cognitoSettings = new CognitoSettings(LaunchActivity.this);
+            System.out.println("JWT Access Token: "+String.valueOf(userSession.getAccessToken().getJWTToken()));
+            userDetails = new UserDetails(userSession.getUsername(),userSession.getAccessToken().toString(),userSession.getRefreshToken().toString());
+            sessionManager.createSession(email, password,userSession.getAccessToken().getJWTToken());
+        }
+
+        @Override
+        public void getAuthenticationDetails(AuthenticationContinuation authenticationContinuation, String userId) {
+            System.out.println("in getAuthentication");
+
+            AuthenticationDetails authenticationDetails = new AuthenticationDetails(userId, password,null);
+
+            authenticationContinuation.setAuthenticationDetails(authenticationDetails);
+
+            authenticationContinuation.continueTask();
+        }
+
+        @Override
+        public void getMFACode(MultiFactorAuthenticationContinuation continuation) {
+            System.out.println("in getMFACode");
+        }
+
+        @Override
+        public void authenticationChallenge(ChallengeContinuation continuation) {
+            System.out.println("in authenticationChallenge");
+        }
+
+        @Override
+        public void onFailure(Exception exception) {
+            System.out.println("Error : "+exception.getLocalizedMessage());
+        }
+    };
+
+    private void login(String email,String password) {
+        CognitoSettings cognitoSettings = new CognitoSettings(LaunchActivity.this);
+        CognitoUser thisUser = cognitoSettings.getUserPool()
+                .getUser(String.valueOf(email));
+        thisUser.getSessionInBackground(authenticationHandler);
     }
 }
--- app/src/main/java/com/example/myottapp/models/SessionManager.java
+++ app/src/main/java/com/example/myottapp/models/SessionManager.java
@@ -21,6 +21,7 @@ public class SessionManager {
     private static final String LOGIN = "IS_LOGIN";
 //    public static final String NAME = "NAME";
     public static final String EMAIL = "EMAIL";
+    public static final String PASSWORD = "PASSWORD";
     public static final String ACCESS_TOKEN = "JWT Token";
 //    public static final String ID = "ID";
 
@@ -30,19 +31,11 @@ public class SessionManager {
         editor = sharedPreferences.edit();
     }
 
-//    public void createSession(String name, String email, String id){
-//
-//        editor.putBoolean(LOGIN, true);
-//        editor.putString(NAME, name);
-//        editor.putString(EMAIL, email);
-//        editor.putString(ID, id);
-//        editor.apply();
-//
-//    }
-    public void createSession(String email, String accessToken){
+    public void createSession(String email, String password, String accessToken){
 
         editor.putBoolean(LOGIN, true);
         editor.putString(EMAIL, email);
+        editor.putString(PASSWORD,password);
         editor.putString(ACCESS_TOKEN,accessToken);
         DataModel.accessToken=accessToken;
         editor.apply();
@@ -52,18 +45,28 @@ public class SessionManager {
         return sharedPreferences.getBoolean(LOGIN, false);
     }
 
-    public void checkLogin(){
+    public static String getEMAIL() {
+        return EMAIL;
+    }
+
+    public static String getPASSWORD() {
+        return PASSWORD;
+    }
+
+    public boolean checkLogin(){
 
         if (!this.isLoggin()){
             Intent i = new Intent(context, LoginActivity.class);
             context.startActivity(i);
             ((LaunchActivity) context).finish();
+            return false;
         }else
         {
             DataModel.accessToken=sharedPreferences.getString(ACCESS_TOKEN,null);
             Intent i = new Intent(context, MainActivity.class);
             context.startActivity(i);
             ((LaunchActivity) context).finish();
+            return true;
         }
     }
 
@@ -72,6 +75,7 @@ public class SessionManager {
         HashMap<String, String> user = new HashMap<>();
 //        user.put(NAME, sharedPreferences.getString(NAME, null));
         user.put(EMAIL, sharedPreferences.getString(EMAIL, null));
+        user.put(PASSWORD, sharedPreferences.getString(PASSWORD, null));
 //        user.put(ID, sharedPreferences.getString(ID, null));
 
         return user;
